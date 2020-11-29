7a459e3b2ebe1fb991902995b13dc626e449b38c
fepu08 <besenyeiferenc@protonmail.com>
2020-11-29 12:38:08 +0100

    using assertEquals(expected, actual)
    Write tests for StringHelper.truncateAnInFiesrt2Positions(String) using assertEquals

--- src/test/java/org/fepu08/junit/helper/StringHelperTest.java
+++ src/test/java/org/fepu08/junit/helper/StringHelperTest.java
@@ -3,9 +3,18 @@ package org.fepu08.junit.helper;
 import org.junit.Assert.*;
 import org.junit.Test;
 
+import static org.junit.Assert.assertEquals;
+
 public class StringHelperTest {
     @Test
     public void test(){
-
+        StringHelper helper = new StringHelper();
+        //assertEqueals(excpected, actual)
+        assertEquals("ABC", "ABC");
+        //AACD => CD, ACD => CD, CDEF => CDEF, CDAA => CDAA
+        assertEquals("CD", helper.truncateAInFirst2Positions("AACD"));
+        assertEquals("CD", helper.truncateAInFirst2Positions("ACD"));
+        assertEquals("CDEF", helper.truncateAInFirst2Positions("CDEF"));
+        assertEquals("CDAA", helper.truncateAInFirst2Positions("CDAA"));
     }
 }
