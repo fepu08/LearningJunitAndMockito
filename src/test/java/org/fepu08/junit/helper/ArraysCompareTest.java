35ed0fd169e265f898118b887bf47aaab609b085
fepu08 <ferencbesenyei@protonmail.com>
2020-11-29 14:20:40 +0100

    Testing performance with @Test(timeout=milliseconds)
    its a good test if you want to set strict performance requirement

--- src/test/java/org/fepu08/junit/helper/ArraysCompareTest.java
+++ src/test/java/org/fepu08/junit/helper/ArraysCompareTest.java
@@ -24,4 +24,18 @@ public class ArraysCompareTest {
         int[] numbers = null;
         Arrays.sort(numbers);
     }
+
+    /**
+     * Testing performance
+     * @Test(timeout=milliseconds)
+     * its a good test if you want to set strict performance requirement
+     * */
+    @Test(timeout=100)
+    public void testSort_Performance(){
+        int array[] = {12,23,4};
+        for (int i = 1; i <= 1000000; i++){
+            array[0] = i;
+            Arrays.sort(array);
+        }
+    }
 }
