6bc07f300074643dcb3a08ef6752c6b49c314b61
fepu08 <ferencbesenyei@protonmail.com>
2020-11-29 19:47:34 +0100

    edit conf files and create new test class

---
+++ Learning-Mockito/src/test/java/org/fepu08/FirstMockitoTest.java
@@ -0,0 +1,12 @@
+package org.fepu08;
+
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+public class FirstMockitoTest {
+    @Test
+    public void test(){
+        assertTrue(true);
+    }
+}
