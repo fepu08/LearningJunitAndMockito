8548196d7e16ee44b34602a76161f7a2502e2726
fepu08 <ferencbesenyei@protonmail.com>
2020-11-29 14:11:56 +0100

    Compare arrays with assertArrayEquals

---
+++ src/test/java/org/fepu08/junit/helper/ArraysCompareTest.java
@@ -0,0 +1,21 @@
+package org.fepu08.junit.helper;
+
+import org.junit.Test;
+
+import java.util.Arrays;
+
+import static org.junit.Assert.*;
+
+public class ArraysCompareTest {
+
+    // Arrays.sort
+    @Test
+    public void test(){
+        int[] numbers = {12,3,4,1};
+        int[] expected = {1,3,4,12};
+        Arrays.sort(numbers);
+        // assertEquals() wont work with arrays
+        // because arrays are reference type (objects)
+        assertArrayEquals(expected, numbers);
+    }
+}
