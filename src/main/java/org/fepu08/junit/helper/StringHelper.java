d609ca3162851eb46028e1561685310bbd1a0e47
fepu08 <besenyeiferenc@protonmail.com>
2020-11-29 11:56:12 +0100

    Created StringHelperTest class
    import junit.Assert.*

--- src/main/java/org/fepu08/junit/helper/StringHelper.java
+++ src/main/java/org/fepu08/junit/helper/StringHelper.java
@@ -2,6 +2,7 @@ package org.fepu08.junit.helper;
 
 public class StringHelper {
 
+    //AACD => CD, ACD => CD, CDEF => CDEF, CDAA => CDAA
     public String truncateAInFirst2Positions(String str) {
         if (str.length() <= 2)
             return str.replaceAll("A", "");
---
+++ src/test/java/org/fepu08/junit/helper/StringHelperTest.java
@@ -0,0 +1,11 @@
+package org.fepu08.junit.helper;
+
+import org.junit.Assert.*;
+import org.junit.Test;
+
+public class StringHelperTest {
+    @Test
+    public void test(){
+
+    }
+}
